436bcf2988524d467131b1ba93cd42fbaf436600
Muhammad Beni Fajri <m.b.fajrii@gmail.com>
2024-02-07 15:14:14 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:chatting_app/pages/chat_page.dart';
+import 'package:chatting_app/pages/login_page.dart';
+import 'package:chatting_app/pages/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: LoginPage.id,
+      routes: {
+        LoginPage.id: (context) => const LoginPage(),
+        RegisterPage.id: (context) => const RegisterPage(),
+        ChatPage.id: (context) => const ChatPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/chat_page.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class ChatPage extends StatefulWidget {
+  static const String id = 'chat_page';
+
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Room'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.close),
+            tooltip: 'Logout',
+            onPressed: () async {
+              final navigator = Navigator.of(context);
+              await _auth.signOut();
+
+              navigator.pushReplacementNamed(LoginPage.id);
+            }
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Expanded(
+              child: Placeholder(),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                MaterialButton(
+                  color: Theme.of(context).primaryColor,
+                  textTheme: ButtonTextTheme.primary,
+                  onPressed: () {},
+                  child: const Text('SEND'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
