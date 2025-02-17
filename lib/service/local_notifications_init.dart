284e2b7adcb8c65e5cc839581df478f7a7251f72
Muhammad Beni Fajri <m.b.fajrii@gmail.com>
2025-02-17 14:28:59 +0700

    fix : flutter local notification error

--- lib/service/local_notifications_init.dart
+++ lib/service/local_notifications_init.dart
@@ -101,17 +101,17 @@ Future<void> initializeNotification() async {
     requestAlertPermission: false,
     requestBadgePermission: false,
     requestSoundPermission: false,
-    onDidReceiveLocalNotification:
-        (int id, String? title, String? body, String? payload) async {
-      didReceiveLocalNotificationStream.add(
-        ReceivedNotification(
-          id: id,
-          title: title,
-          body: body,
-          payload: payload,
-        ),
-      );
-    },
+    // onDidReceiveLocalNotification:
+    //     (int id, String? title, String? body, String? payload) async {
+    //   didReceiveLocalNotificationStream.add(
+    //     ReceivedNotification(
+    //       id: id,
+    //       title: title,
+    //       body: body,
+    //       payload: payload,
+    //     ),
+    //   );
+    // },
     notificationCategories: darwinNotificationCategories,
   );
   final LinuxInitializationSettings initializationSettingsLinux =
